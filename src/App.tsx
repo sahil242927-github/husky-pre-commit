9ee99311481bc24f87cfb36617aca74722d85fde
sahil242927.github@gmail.com <sahil242927.github@gmail.com>
2022-07-13 15:22:08 +0530

    IO-1234 test: skdfj

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload. added pre push hook
+          <span>Test</span>
         </p>
         <a
           className="App-link"
